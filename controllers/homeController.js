21a4ada9700fed24da8aec96acce62ff6b33460b
michael.dang <michael.dang@workday.com>
2017-04-06 21:08:02 -0700

    made summoner compare lib to remove clutter in home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,7 @@ var request = require('request');
 var lolAPI = require('./lolAPIController');
 var Promise = require('promise')
 var championMap = require('../public/static/champions.json');
+var summonerComparer = require('../lib/summonerComparer')
 var router = express.Router();
 
 
@@ -20,8 +21,8 @@ router.post('/', function(req, res) {
             var summonerStats2 = results[1]
             var commonChampions = findCommonChampions(summonerStats, summonerStats2)
 
-            var compareChampionsResult = compareCommonChampions(commonChampions);
-            displayComparisonResults(summonerName, summonerName2, compareChampionsResult);
+            var compareChampionsResult = summonerComparer.compareCommonChampions(commonChampions);
+            summonerComparer.displayComparisonResults(summonerName, summonerName2, compareChampionsResult);
             res.render('results', { summonerA: summonerName, summonerB: summonerName2, winner: compareChampionsResult.compare, resultList:compareChampionsResult.championCompareList });
         }
         else {
@@ -78,67 +79,5 @@ var findCommonChampions = function(championsA, championsB) {
     }
     return commonChampions
 }
-
-var compareCommonChampions = function(commonChampions) {
-    var compareValue = 0;
-    var championCompareList = commonChampions.map( ([championA, championB]) => {
-        var championCompare = compareChampionStats(championA.stats, championB.stats);
-        compareValue += championCompare.compare;
-        return ({
-            championName: championMap[championA.id],
-            result: championCompare
-        });
-    });
-
-    var ret = {
-        compare : compareValue,  // overall result for all the champions.
-        // comparison for each champion.
-        championCompareList: championCompareList // array will consist of json like {id: championId, result: return value from compareChampionStats}
-    }
-    return ret;
-}
-
-var compareChampionStats = function(statsA, statsB) {
-    var winCmp = statsA.totalSessionsWon - statsB.totalSessionsWon;
-    var cmp = (winCmp > 0 ? 1 : (winCmp < 0 ? -1 : 0));
-    var championCompare = {
-        compare: cmp,
-        reason: {
-            winCompare: winCmp
-            // More things will go in here eventually
-        }
-    };
-
-    return championCompare;
-}
-
-// takes the return value from compareCommonChampions
-var displayComparisonResults = function(summoner1, summoner2, compareChampionsResult) {
-    if (compareChampionsResult.compare > 0) {
-        console.log("" + summoner1 + " wins!");
-    }
-    else if (compareChampionsResult.compare < 0) {
-        console.log("" + summoner2 + " wins!");
-    }
-    else {
-        console.log("It's a tie. " + summoner1 + " and " + summoner2 + " are both equally trash!");
-    } 
-
-    console.log("Results for each champion are as follows:");
-    compareChampionsResult.championCompareList.forEach(champion => {
-        console.log("\tChampion id " + champion.championName);
-        if (champion.result.compare == 0) {
-            console.log("\t\tTie.");
-        }
-        else {
-            var championWinner = champion.result.compare > 0 ? summoner1 : summoner2;
-            console.log("\t\t" + championWinner + " won by " + Math.abs(champion.result.reason.winCompare) + " wins.");
-        }
-    });
-}
-
-// router.get('/form', function(req, res) {
-//     res.render('form', { title: 'Express' });
-// });
 
 module.exports = router;
