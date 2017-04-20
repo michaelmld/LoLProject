2f6b946d23527b2320dbb2413afa2121f9cdaddc
Troy Pocklington <tdashroy@gmail.com>
2017-04-20 07:50:45 -0700

    Added directory for models and a Summoner class which corresponds to SummonerDTO from Riot's APIs

---
+++ models/Summoner.js
@@ -0,0 +1,11 @@
+module.exports = class Summoner {
+    constructor() {
+        this.profileIconId = undefined;
+        this.name = undefined;
+        this.summonerLevel = undefined;
+        this.revisionDate = undefined;
+        this.id = undefined;
+        this.accountId = undefined;
+    }
+}
+
